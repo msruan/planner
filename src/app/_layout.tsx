78c8064196d2146e47c529de83843dbfde701d6c
Ruan <santosruan2021@gmail.com>
2024-07-12 10:48:44 -0300

    feat: tudo funfando, fontes adicionadas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,11 @@ import {Slot} from "expo-router";
 // Import your global CSS file
 import "../global.css"
 import {View} from "react-native";
+import {useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold} from "@expo-google-fonts/inter";
+import Loading from "@/src/components/ui/loading";
 function Layout(){
-  return <View className={"flex-1"}><Slot/></View>
+  const [fontIsLoaded] = useFonts({Inter_400Regular, Inter_500Medium, Inter_600SemiBold})
+  if(!fontIsLoaded){return <Loading/>}
+  return <View className={"flex-1 bg-zinc-950"}><Slot/></View>
 }
 export default Layout;
---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,6 @@
+import {ActivityIndicator} from "react-native";
+
+function Loading(){
+    return <ActivityIndicator className={"text-lime-300 bg-zinc-950 flex-1 justify-center items-center"}/>
+}
+export default Loading;
